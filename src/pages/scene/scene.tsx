0fb0b06f54a7e979700f932505256d654c88e4ae
MURAMASA2470 <keisuke226214@gmail.com>
2022-06-05 21:37:51 +0900

    余分な隙間を無くしました

--- src/pages/scene/scene.tsx
+++ src/pages/scene/scene.tsx
@@ -5,11 +5,11 @@ import styles from '../../assets/css/scene/scene.module.css';
 import Palette, { PalletType } from '../../components/palette/Palette';
 
 const data: PalletType[] = [
-  { title: "Motion",  content: <></>, width: "22.5vw", height: "45vh" },
+  { title: "Motion",  content: <></>, width: "22.5vw", height: "45.5vh" },
   { title: "Main",    content: <></>, width: "50vw", height: "50vh" },
-  { title: "Flow",    content: <></>, width: "25vw", height: "91vh" },
-  { title: "Media",   content: <></>, width: "22.5vw", height: "45vh" },
-  { title: "Mitei",   content: <></>, width: "50vw", height: "40vh" },
+  { title: "Flow",    content: <></>, width: "25vw", height: "92vh" },
+  { title: "Media",   content: <></>, width: "22.5vw", height: "45.5vh" },
+  { title: "Mitei",   content: <></>, width: "50vw", height: "41vh" },
 ];
 
 const Scene: Component = () => {
