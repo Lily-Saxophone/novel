027d1a7f93318dd2806590ef7faefc4c47393cb1
MURAMASA2470 <keisuke226214@gmail.com>
2022-06-11 11:02:34 +0900

    Y方向スクロールバー抑制

--- src/pages/scene/scene.tsx
+++ src/pages/scene/scene.tsx
@@ -1,13 +1,21 @@
-import type { Component } from 'solid-js';
+import type { Component, JSX } from 'solid-js';
 import Masonry from "solid-masonry";
 
 import styles from '../../assets/css/scene/scene.module.css';
 import Palette, { PalletType } from '../../components/palette/Palette';
 
+const flow: JSX.Element = (
+  <>
+    <div>
+      
+    </div>
+  </>
+)
+
 const data: PalletType[] = [
   { title: "Motion",  content: <></>, width: "22.5vw", height: "45.5vh" },
   { title: "Main",    content: <></>, width: "50vw", height: "50vh" },
-  { title: "Flow",    content: <></>, width: "25vw", height: "92vh" },
+  { title: "Flow",    content: flow, width: "25vw", height: "calc(91vh + 10px)" },
   { title: "Media",   content: <></>, width: "22.5vw", height: "45.5vh" },
   { title: "Mitei",   content: <></>, width: "50vw", height: "41vh" },
 ];
