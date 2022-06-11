ed67ce1760ef3b3017205123e7eb301d51c2c82f
MURAMASA2470 <keisuke226214@gmail.com>
2022-06-11 12:37:48 +0900

    FLOWの作成

--- src/pages/scene/scene.tsx
+++ src/pages/scene/scene.tsx
@@ -3,19 +3,12 @@ import Masonry from "solid-masonry";
 
 import styles from '../../assets/css/scene/scene.module.css';
 import Palette, { PalletType } from '../../components/palette/Palette';
-
-const flow: JSX.Element = (
-  <>
-    <div>
-      
-    </div>
-  </>
-)
+import SceneFlow from '../../components/scene/SceneFlow';
 
 const data: PalletType[] = [
-  { title: "Motion",  content: <></>, width: "22.5vw", height: "calc(45.5vh - 13px)" },
-  { title: "Main",    content: <></>, width: "50vw", height: "calc(50vh - 13px)" },
-  { title: "Flow",    content: flow, width: "25vw", height: "calc(91vh + 10px - 13px)" },
+  { title: "Motion",  content: <></>, width: "22.5vw", height: "calc(45.5vh - 10px - 13px)" },
+  { title: "Main",    content: <></>, width: "50vw", height: "calc(50vh - 10px  - 13px)" },
+  { title: "Flow",    content: <SceneFlow />, width: "25vw", height: "calc(91vh - 13px)" },
   { title: "Media",   content: <></>, width: "22.5vw", height: "45.5vh" },
   { title: "Mitei",   content: <></>, width: "50vw", height: "41vh" },
 ];
@@ -30,7 +23,7 @@ const Scene: Component = () => {
     >
       {(item: PalletType) => (
         <Palette width={item.width} height={item.height}>
-          {item.title}
+          {item.content}
         </Palette>
       )}
     </Masonry>
